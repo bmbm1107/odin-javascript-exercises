6bb7826b31135b1c30d6909023b83840581fb1f8
bmbm1107 <marczisbalazs@gmail.com>
2023-11-14 16:17:52 +0100

    implement solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
 
+
+const repeatString = function(string, num) {
+
+    if (num < 0) return 'ERROR'
+
+    message = ''
+
+    for (let i=0; i < num; i++){
+
+    message = message.concat(string)
+
+    }
+
+    return message
 };
 
 // Do not edit below this line
