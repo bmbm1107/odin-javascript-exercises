f1581987d0224e9670a271a8567a4e3dcd746dcc
bmbm1107 <marczisbalazs@gmail.com>
2023-11-30 22:14:31 +0100

    implement solutions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (a) {
+  return a.reduce((arg, item) => {
+    return arg + item
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function (a) {
+  return a.reduce((arg, item) => {
+    return arg * item
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+
+  if (b === 0) return 1
+
+  let power = a
+ 
+  while (b > 1) {
+   power *= a;
+   b--;
+  }
+
+  return power
 };
 
-const factorial = function() {
-	
+// this is clearly problematic, cause of the default return value
+const factorial = function (n) {
+  if (n === 0) return 1
+  let arrOfNum = []
+
+  while (n > 1) {
+  arrOfNum.push(n);
+  n--;
+  }
+
+  return arrOfNum.reduce((arg, item) => {
+    return arg * item
+  }, 1)
+
 };
 
 // Do not edit below this line
