41c7d8e4815ce8b022449f3c23b243e16d0e8fc0
bmbm1107 <marczisbalazs@gmail.com>
2023-12-01 11:21:34 +0100

    add sollution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,10 @@
-const palindromes = function () {
+const palindromes = function (aString) {
+    let clearString = aString.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,' ').toLowerCase()
+    let letters = clearString.split('').filter((letter) => (letter !== ' '))
+    let reverseLetters = letters.map((x) => x).reverse()
+   
+    return letters.every((letter, index) => (letter === reverseLetters[index]))
+
 
 };
 
